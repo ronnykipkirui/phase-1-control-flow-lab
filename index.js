f420170dff20c88c65edb48e785bc2ddae857131
Ronny <ronnybet41@gmail.com>
2022-08-19 18:46:56 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if(ride <=400){
+     return "This one is on me!"
+  }
+  else if(ride>2000 && ride<2500){
+    return 'I will gladly take your thirty bucks.'
+  }
+  else{
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  // city=='NYC'? 'Ok, sounds good.':'No go.'
+// const city=NYC
+if(city==='NYC'){
+  return "Ok, sounds good."
+}
+else{
+  return "No go."
+}
+  // city === 'NYC'? "Ok, sounds good.": "No go."
+  
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+ 
+  switch(tip){
+    case 'generous':
+    return 'Thank you so much.'
+    
+    case 'not as generous':
+    return 'Thank you.'
+
+    default:
+      return 'Bye.'
+  }
+
+
 }
